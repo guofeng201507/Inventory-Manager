f75b087b0b46a66ed8242aa63b9a022e68900f9b
guofeng201507 <guofeng20094@gmail.com>
2021-08-23 01:31:48 +0800

    udpate

--- flaskinventory/__init__.py
+++ flaskinventory/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '323b22caac41acbf'
-app.config['SQLALCHEMY'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 from flaskinventory import routes
